838f36a5c50ca65b57f098180b665f037320653a
Matteo Portella <devportella@gmail.com>
2025-10-07 11:46:10 +0100

    feat: add main menu with navigation and method structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("What do you wish to do?\n");
+    Console.WriteLine("1 - Open file");
+    Console.WriteLine("2 - Create new file");
+    Console.WriteLine("0 - Exit\n");
+    Console.Write("Your input: ");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default: Menu(); break;
+    }
+}
+
+static void Open()
+{
+    
+}
+
+static void Edit()
+{
+
+}
